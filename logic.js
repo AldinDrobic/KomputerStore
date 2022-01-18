ceb23535516df5133e66947317abc0f649dec410
Aldin Drobic <aldin.drobic@se.experis.com>
2022-01-18 17:16:46 +0100

    CHORE: Added get loan btn, gui+logic

--- logic.js
+++ logic.js
@@ -33,3 +33,44 @@ function transferPayToBank(){
     return document.getElementById("balance").innerText = balance;
 
 }
+
+function getLoan()
+{
+    if (isRepayLoan == false)
+    {
+            let loanInput = prompt("How big a loan do you want?"); //Promp windows opens and lets the user type in loan amount.
+            loanInput = parseInt(loanInput); //Parsing so I will get correct format.
+            checkNaN = Number.isInteger(loanInput);//Alerting user that they didn't enter a digit.
+            if (checkNaN == false || loanInput < 1) 
+            {
+                alert("You must enter a valid number.")
+            }
+       
+            do 
+            {//I want to do this until the user adds a correct loan amount.
+                if ((loanInput) > (balance * 2))
+                { //Maximum loan is double the amount in bank. We check that Joe cannot loan more the double the amount.
+                    alert("You can't loan more than double your bank account. Maximum loan amount for you now is " + (balance * 2));
+                    loanInput = prompt("How big a loan do you want?"); //Promp windows opens and lets the user type in loan amount.
+                    loanInput = parseInt(loanInput); //Parsing so I will get correct format.
+                    checkNaN = Number.isInteger(loanInput);//Alerting user that they didn't enter a digit.
+                    if (checkNaN == false) 
+                    {
+                        alert("You must enter a valid number.")
+                    }
+                }
+            } while ((loanInput) > (balance * 2));
+            
+            if ((loanInput) <= (balance * 2) && loanInput > 0) //If the loan input is acceptable then we will be adding it to the outstanding loan.
+            { 
+                outstandingLoan = loanInput; //Adding loan to the outstanding loan.
+                loanInput = 0; //resetting loan.
+                isRepayLoan = true; //Set bool to true so that user cant make new loan until this one is repaid.
+                return document.getElementById("outstandingLoan").innerHTML = outstandingLoan; //Adding loan to outstanding loan.
+            }               
+    }
+    else 
+    {
+         alert("You must repay your loan before you can make a new one!");
+    }       
+}

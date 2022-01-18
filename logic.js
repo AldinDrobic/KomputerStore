d7299654c18131b322671957b342d90d7f13f03e
Aldin Drobic <aldin.drobic@se.experis.com>
2022-01-18 19:02:09 +0100

    CHORE: Updated logic on get loan + bank btn

--- logic.js
+++ logic.js
@@ -17,12 +17,11 @@ function transferPayToBank()
             outstandingLoan -= reservedDeduction; //Here is where the deduction is subtracted with the outstanding loan.
             if (outstandingLoan < 0) //If deduction is bigger than outstanding loan (outstanding loan = 10 && reservedDeduction == 15) then it will be negative. If this happens we want to return the money that is overflowing to Joe.
             {
-                pay += (outstandingLoan * (-1)) //Multiply with -1 so the negative number will be positive. Then add it to the bank.
-                isRepayLoan = false; //Set bool to false so the user can make a new loan.
+                pay += (outstandingLoan * (-1)) //Multiply with -1 so the negative number will be positive. Then add it to the bank.                
             }
-            if(outstandingLoan = 0)
+            if(outstandingLoan <= 0)//If we don't have any loan we will set the bool to false here.
             {
-                isRepayLoan = false;
+                isRepayLoan = false; //Set bool to false so the user can make a new loan.
             }
         }
     }
@@ -31,7 +30,6 @@ function transferPayToBank()
         pay = 0;
         return document.getElementById("pay").innerText = pay;
     }
-
     repayLoanByPay(); //Must run before resetPay() beacuse if Joe has an outstanding loan 10% of the pay must first be reserved.
     balance += pay; //Must run down here beacuse of conditions.
     resetPay(); //Set the pay to 0.
@@ -51,29 +49,22 @@ function getLoan()
             {
                 alert("You must enter a valid number.")
             }
-       
-            do 
-            {//I want to do this until the user adds a correct loan amount.
-                if ((loanInput) > (balance * 2))
-                { //Maximum loan is double the amount in bank. We check that Joe cannot loan more the double the amount.
-                    alert("You can't loan more than double your bank account. Maximum loan amount for you now is " + (balance * 2));
-                    loanInput = prompt("How big a loan do you want?"); //Promp windows opens and lets the user type in loan amount.
-                    loanInput = parseInt(loanInput); //Parsing so I will get correct format.
-                    checkNaN = Number.isInteger(loanInput);//Alerting user that they didn't enter a digit.
-                    if (checkNaN == false) 
-                    {
-                        alert("You must enter a valid number.")
-                    }
-                }
-            } while ((loanInput) > (balance * 2));
-            
+
+            if ((loanInput) > (balance * 2))//Maximum loan is double the amount in bank. We check that Joe cannot loan more the double the amount.
+            { 
+                alert("You can't loan more than double your bank account. Maximum loan amount for you now is " + (balance * 2));
+            }
+
             if ((loanInput) <= (balance * 2) && loanInput > 0) //If the loan input is acceptable then we will be adding it to the outstanding loan.
             { 
                 outstandingLoan = loanInput; //Adding loan to the outstanding loan.
+                balance += loanInput;
                 loanInput = 0; //resetting loan.
                 isRepayLoan = true; //Set bool to true so that user cant make new loan until this one is repaid.
-                return document.getElementById("outstandingLoan").innerText = outstandingLoan; //Adding loan to outstanding loan.
-            }               
+                document.getElementById("outstandingLoan").innerText = outstandingLoan; //Adding loan to outstanding loan.
+                document.getElementById("balance").innerText = balance;
+            }              
+            console.log(isRepayLoan);
     }
     else 
     {

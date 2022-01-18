1ca9ef2b16f728453b22b711a393d03039d40ed3
Aldin Drobic <aldin.drobic@se.experis.com>
2022-01-18 18:43:59 +0100

    CHORE: Updated logic bank button

--- logic.js
+++ logic.js
@@ -8,29 +8,35 @@ function incrementPay(){
     return document.getElementById("pay").innerText = pay;
 }
 
-function transferPayToBank(){
-    balance += pay;
-    
-    function resetPay(){ //Resets pay to 0 after the amount has been transfered to bank.
-        pay = 0;
-        return document.getElementById("pay").innerText = pay;
-    }
-
-    function repayLoanByPay(){//If we have a loan this function will repay the loan when the pay is transfered from pay to bank.
+function transferPayToBank()
+{
+    function repayLoanByPay()//If we have a loan this function will repay the loan when the pay is transfered from pay to bank.
+    {
         if (isRepayLoan){
             let reservedDeduction = 0.1 * pay; //Deduction is where the 10% of the pay amount is saved so it can be reserved for the outstanding loan.
             outstandingLoan -= reservedDeduction; //Here is where the deduction is subtracted with the outstanding loan.
             if (outstandingLoan < 0) //If deduction is bigger than outstanding loan (outstanding loan = 10 && reservedDeduction == 15) then it will be negative. If this happens we want to return the money that is overflowing to Joe.
             {
-                outstandingLoan = (outstandingLoan * (-1)) //Multiply with -1 so the negative number will be positive. Then add it to the bank.
+                pay += (outstandingLoan * (-1)) //Multiply with -1 so the negative number will be positive. Then add it to the bank.
                 isRepayLoan = false; //Set bool to false so the user can make a new loan.
+            }
+            if(outstandingLoan = 0)
+            {
+                isRepayLoan = false;
             }
         }
     }
+    function resetPay()//Resets pay to 0 after the amount has been transfered to bank.
+    { 
+        pay = 0;
+        return document.getElementById("pay").innerText = pay;
+    }
 
     repayLoanByPay(); //Must run before resetPay() beacuse if Joe has an outstanding loan 10% of the pay must first be reserved.
+    balance += pay; //Must run down here beacuse of conditions.
     resetPay(); //Set the pay to 0.
-    return document.getElementById("balance").innerText = balance;
+    document.getElementById("outstandingLoan").innerText = outstandingLoan;
+    document.getElementById("balance").innerText = balance;
 
 }
 
@@ -66,11 +72,33 @@ function getLoan()
                 outstandingLoan = loanInput; //Adding loan to the outstanding loan.
                 loanInput = 0; //resetting loan.
                 isRepayLoan = true; //Set bool to true so that user cant make new loan until this one is repaid.
-                return document.getElementById("outstandingLoan").innerHTML = outstandingLoan; //Adding loan to outstanding loan.
+                return document.getElementById("outstandingLoan").innerText = outstandingLoan; //Adding loan to outstanding loan.
             }               
     }
     else 
     {
          alert("You must repay your loan before you can make a new one!");
     }       
+}
+
+function repayLoan(){
+    if (pay < outstandingLoan)
+    {
+        alert("You have insufficient pay, go and work a little bit.")
+    }
+    else
+    {
+        outstandingLoan = document.getElementById("outstandingLoan").innerText;
+        pay -= outstandingLoan;
+        outstandingLoan = 0;
+        isRepayLoan = false; //Set bool to true so that user cant make new loan until this one is repaid.
+        setPay();
+        return document.getElementById("outstandingLoan").innerText = outstandingLoan;       
+    }
+   
+}
+
+function setPay()//Setting value of pay
+{ 
+    return document.getElementById("pay").innerHTML = pay;
 }
